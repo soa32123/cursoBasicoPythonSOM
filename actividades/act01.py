185b278f44d4ce35b9c041b261b135a50d44934b
soa <myowndata_@outlook.com>
2023-02-13 12:55:28 +0100

    ejemplos y actividades

--- actividades/act01.py
+++ actividades/act01.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3.11
+
+# Script realizado por: 
+
+# Descripción: Script que solicita dos números y muestra la suma de ellos
+
+num1 = input("Indique el primer número:")
+# Véase alternativa --> num1 = int(input("Indique el primer número:"))
+num2 = input("Indique el segundo números:")
+suma = num1 + num2
+print(suma)
+
+suma = int(num1) + int(num2)
+print(suma)
--- ejemplos/ej07.py
+++ ejemplos/ej07.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3.11
+
+cad = "Hola mundo"
+edad = 18
+nota = 10
+print(cad)
+print(cad[0])
+print(cad[2:5])
+print(cad*2)
+print(cad + ", añado") # Concatenamos cadenas
