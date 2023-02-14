d1531a1e3b1d7cc6a2abbc3591619fa205115af3
Sergio <myowndata_@outlook.com>
2023-02-14 13:02:16 +0100

    Algunas actividades...

---
+++ actividades/act01b.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.11
+
+# Realiza de nuevo la Act01.py pero se debe mostrar
+# además de la suma, la resta, multiplicación y división.
+
+num1 = int(input("Indique el primer número:"))
+num2 = int(input("Indique el segundo números:"))
+
+res = num1 + num2
+print("La suma de " , num1 , " y " , num2 , " = " , res)
+
+res = num1 - num2
+print("La resta de " , num1 , " y " , num2 , " = " , res)
+
+res = num1 * num2
+print("La multiplicación de " , num1 , " y " , num2 , " = " , res)
+
+res = num1 / num2
+print("La división de " , num1 , " y " , num2 , " = " , res)
--- actividades/act02.py
+++ actividades/act02.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3.11
+
+# Realiza un formulario que muestre por pantalla los datos
+# de manera ordenada que se pregunten a continuación:
+# – Nombre
+# – Apellidos
+# – Edad
+# – Dirección y
+# – Peso
+
+nombre = input("Dime tu nombre: ")
+apellido = input("Indícame tu primer apellido: ")
+edad = int(input("Dime tu edad: "))
+direccion = input("Dime tu dirección: ")
+peso = int(input("Indícame tu peso: "))
+
+print("Tu nombre es: " , nombre ,  apellido , " tienes " , edad , " años y pesas ", peso , "Kg. Vives en ", direccion,".")
+
